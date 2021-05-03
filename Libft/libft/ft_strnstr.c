38ad2beb221bf5f76b130509a1b8f199c52ea5e6
gpark <gpark@student.42seoul.kr>
2021-05-03 22:02:45 +0900

    libft part1 half

---
+++ Libft/libft/ft_strnstr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpark <gpark@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/30 02:37:24 by gpark             #+#    #+#             */
+/*   Updated: 2021/05/03 21:21:54 by gpark            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static	int	find_from_str(char *haystack, char *needle)
+{
+	int		i;
+
+	while (*(needle + i) != 0)
+	{
+		if (*(haystack + i) != *(needle + i))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+char		*ft_strnstr(char *haystack, char *needle, size_t len)
+{
+	size_t	idx;
+
+	idx = 0;
+	if (*needle == 0)
+		return (haystack);
+	while (*(haystack + idx) != 0 && idx < len)
+	{
+		if (find_from_str(haystack + idx, needle) == 1)
+		{
+			return (haystack + idx);
+		}
+		idx++;
+	}
+	return (0);
+}
