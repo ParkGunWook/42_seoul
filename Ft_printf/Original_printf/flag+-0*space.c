5264dd1b7e96b30b715a0fd94db6d140827b328a
gpark <gpark@student.42seoul.kr>
2021-05-06 19:00:19 +0900

    stdarg studying

---
+++ Ft_printf/Original_printf/flag+-0*space.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+	int i1 = 123456;
+	int i2 = -123456;
+	int *ptr_i1 = &i1;
+	char s1[10] = "abcdef";
+	char s2[10] = "ghijkl";
+	printf("%d %d\n", i1, i2);
+	printf("%+d %+d | what solo plus works\n", i1, i2);
+	printf("% d % d | what solo space works\n", i1, i2);
+	printf("%-d %-d | what solo minus works\n", i1, i2);
+	printf("%4d %4d | if num over width\n\n", i1, i2);
+	printf("%12d %12d | + and width\n", i1, i2);
+	printf("%+12d %+12d | + and width\n", i1, i2);
+	printf("%-12d %-12d | - and width\n", i1, i2);
+	printf("%012d %012d | 0 and width\n", i1, i2);
+	printf("%+012d %+012d | + and 0 and width\n", i1, i2);
+	printf("%+-12d %+-12d | + and - and width\n", i1, i2);
+	printf("% 012d % 012d | + and 0 and width\n", i1, i2);
+	printf("% -12d % -12d | space and - and width\n", i1, i2);
+	//printf("abc%-n\n", ptr_i1);
+	//printf("%+s %+s | what solo plus works\n", s1, s2);
+	//printf("% s % s | what solo space works\n", s1, s2);
+	printf("%-s %-s | what solo minus works\n", s1, s2);
+	printf("%12s %12s | pad and minus works\n", s1, s2);
+	printf("%-12s %-12s | what pad solo minus works\n", s1, s2);
+	//printf("%+012s %+012s | + and 0 and width\n", s1, s2);
+	//printf("%+-12s %+-12s | + and - and width\n", s1, s2);
+	//printf("% 012s % 012s | + and 0 and width\n", s1, s2);
+	//printf("% -12s % -12s | space and - and width\n", s1, s2);
+	//printf("%-012d %-012d | - and 0 and width\n", i1, i2); //zero is ignored when flag '-'
+	//printf("%0s\n", s1); //0 and ' ' cant'be used in string
+}
---
+++ Ft_printf/Original_printf/spec.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+	char c = 'c';
+	char string[] = "hello, world";
+	char string2[10] = {'a', 'b', 35, 29, 8, 27, '\0'};
+	int minus_integer = -1;
+	int integer = 123;
+	int ret_num = 0;
+	unsigned int ui = 4000000000;
+	int *pointer = &integer;
+	double pi = 3.141592;
+	printf("%c\n", c);
+	ret_num = printf("%s\n", string);
+	printf("ret_num : %d\n", ret_num);
+	printf("%s123\n", string2);
+	printf("%p %p\n", &c, string);
+	printf("%d %d\n", integer, minus_integer);
+	ret_num = printf("%i %i\n", integer, minus_integer);
+	printf("ret_num : %d\n", ret_num);
+	printf("%u %u %u\n", integer, ui, minus_integer);
+	printf("%x\n", integer);
+	ret_num = printf("%X\n", integer);
+	printf("ret_num : %d\n", ret_num);
+	ret_num = printf("%%\n");
+	printf("ret_num : %d\n", ret_num);
+	printf("Bonus\n");
+	printf("abcabc%n\n", pointer);
+	printf("%d\n", *pointer);
+	printf("abc%nabc\n", pointer);
+	printf("%d\n", *pointer);
+	ret_num = printf("%nabcabc\n", pointer);
+	printf("%d\n", *pointer);
+	printf("ret_num : %d\n", ret_num);
+	printf("%s%n\n", string, pointer);
+	printf("%d\n", *pointer);
+	printf("%f\n", pi);
+	printf("%g\n", pi);
+	printf("%e\n", pi);
+	ret_num = printf("");
+	printf("ret_num : %d\n", ret_num);
+	ret_num = printf(NULL);
+	printf("ret_num : %d\n", ret_num);
+}
